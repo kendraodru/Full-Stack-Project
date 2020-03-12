d309c20ac9bc2fcb1bd6404a9af5a1655399d817
Kendra <kmodrunia@ucdavis.edu>
2020-03-11 19:28:36 -0700

    changing my bottom pic set

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2069,55 +2069,86 @@ __webpack_require__.r(__webpack_exports__);
 
 var PicSet = function PicSet() {
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "pic-holder"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "#youthtothepeople"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "you are our people, share your story with us")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "pic-set-wrap"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "pic-1nd-row"
+    className: "one-pic-row"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-1"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1568762297330-66bfa0586400?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/0.jpeg",
     alt: ""
   })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-2"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1505976311692-e64b8bbd6cfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/1.jpeg",
     alt: ""
   })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-3"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1506003094589-53954a26283f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    alt: ""
-  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-3"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1552046122-03184de85e08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/2.jpeg",
     alt: ""
   }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "pic-2nd-row"
+    className: "second-pic-row"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-4"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1533093818119-ac1fa47a6d59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/4.jpeg",
     alt: ""
   })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-5"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1514163061636-02db31852e84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/5.jpeg",
     alt: ""
   })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-6"
+    className: "indivi-set-pic"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1573575155376-b5010099301b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    src: "https://youth-to-the-people-seeds.s3-us-west-1.amazonaws.com/splash/6.jpeg",
     alt: ""
-  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "bottom-pic-7"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    src: "https://images.unsplash.com/photo-1555211652-5c6222f971bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    alt: ""
-  }))));
+  })))));
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (PicSet);
+/* harmony default export */ __webpack_exports__["default"] = (PicSet); // const PicSet = () =>{
+//     return(
+//         <div className="pic-set-wrap">
+//             <div className='pic-1nd-row'>
+//                 <div className="bottom-pic-1">
+//                     <img src="https://images.unsplash.com/photo-1568762297330-66bfa0586400?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//                 <div className="bottom-pic-2">
+//                     <img src="https://images.unsplash.com/photo-1505976311692-e64b8bbd6cfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//                 <div className="bottom-pic-3">
+//                     {/* <img src="https://images.unsplash.com/photo-1503278501277-e50457741130?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt=""/> */}
+//                     {/* <img src="https://images.unsplash.com/photo-1531299244174-d247dd4e5a66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=80" alt=""/> */}
+//                     <img src="https://images.unsplash.com/photo-1506003094589-53954a26283f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//                 <div className="bottom-pic-3">
+//                     {/* <img src="https://images.unsplash.com/photo-1503278501277-e50457741130?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt=""/> */}
+//                     {/* <img src="https://images.unsplash.com/photo-1531299244174-d247dd4e5a66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=80" alt=""/> */}
+//                     <img src="https://images.unsplash.com/photo-1552046122-03184de85e08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//             </div>
+//             <div className='pic-2nd-row'>
+//                 <div className="bottom-pic-4">
+//                     <img src="https://images.unsplash.com/photo-1533093818119-ac1fa47a6d59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                     {/* <img src="https://images.unsplash.com/photo-1512437136892-3d200c4645b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt=""/> */}
+//                 </div>
+//                 <div className="bottom-pic-5">
+//                     <img src="https://images.unsplash.com/photo-1514163061636-02db31852e84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//                 <div className="bottom-pic-6">
+//                     <img src="https://images.unsplash.com/photo-1573575155376-b5010099301b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt=""/>
+//                 </div>
+//                 <div className="bottom-pic-7">
+//                     <img src="https://images.unsplash.com/photo-1555211652-5c6222f971bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+//                     {/* <img src="https://images.unsplash.com/photo-1560179304-6fc1d8749b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt=""/> */}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 /***/ }),
 
