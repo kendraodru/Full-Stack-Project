cc3dfe4696522fbe9026575178200f01ddb6601b
Kendra <kmodrunia@ucdavis.edu>
2020-04-01 20:08:39 -0700

    css loading comp

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -984,8 +984,6 @@ __webpack_require__.r(__webpack_exports__);
 
 var Loading = function Loading() {
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "loading-hamburger"
-  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "loading-screen"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "lds-spinner"
--- frontend/components/loading/spinner.js
+++ frontend/components/loading/spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            <div className='loading-hamburger'></div>
+            {/* <div className='hamburger'></div> */}
             <div className='loading-screen'>
                 <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             </div>
